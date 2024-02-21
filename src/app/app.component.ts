4668822d136022f25f484a9d782d90cd454bdb6d
Paul Theby <paultheby@gmail.com>
2024-02-20 21:10:57 -0600

    fe_fullstack_assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TodoListComponent,NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+
 })
 export class AppComponent {
   title = 'fe_todo_list';
